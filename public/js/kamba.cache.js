0cabfe39013e5547b2b70c34ccfcfa5e00af467d
Emmanuel Bouton <goten4@gmail.com>
2014-08-06 21:35:28 +0200

    Update cache.js

--- public/js/kamba.cache.js
+++ public/js/kamba.cache.js
@@ -1,29 +1,12 @@
 $(document).ready(function() {
 
-    function notifyForCacheRefresh() {
-        $.gritter.add({
-            title: "Mise à jour des données",
-            text: "Les données de l'inventaire PuppetDB ont été mises à jour.",
-            class_name: "gritter-light"
-        });
-    }
+    var match = document.URL.match(/(\/env\/[0-9]+)/);
+    var prefixUri = match ? match[1] : '';
 
-    var previous_refreshed_at = new Date().getTime();
-    var refreshCacheIfNecessary = function () {
-        $.getJSON('/cache', function (data) {
-            var fiveMinutes = 5 * 60 * 1000;
-            var now = new Date().getTime();
-            var refreshed_at = new Date(data.refreshed_at).getTime();
-            if (refreshed_at > previous_refreshed_at) {
-                previous_refreshed_at = refreshed_at;
-                notifyForCacheRefresh();
-            }
-            if (data.status == null || (data.status == 'completed' && (now - refreshed_at) > fiveMinutes)) {
-                $.getJSON('/cache/refresh');
-            }
-        });
-        setTimeout(refreshCacheIfNecessary, 5000);
+    var refreshExpiredCache = function () {
+        $.getJSON(prefixUri + '/refresh-expired-cache');
+        setTimeout(refreshExpiredCache, 5000);
     };
 
-    setTimeout(refreshCacheIfNecessary, 5000);
+    setTimeout(refreshExpiredCache, 5000);
 });
