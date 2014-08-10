db75cb4121a5fa185590c71c8f4fa43ccd2e1342
Emmanuel Bouton <goten4@gmail.com>
2014-08-11 00:51:41 +0200

    ADD clear cache

--- public/js/kamba.cache.js
+++ public/js/kamba.cache.js
@@ -1,12 +1,23 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
     var match = document.URL.match(/(\/env\/[0-9]+)/);
     var prefixUri = match ? match[1] : '';
 
     var refreshExpiredCache = function () {
-        $.getJSON(prefixUri + '/refresh-expired-cache');
-        setTimeout(refreshExpiredCache, 5000);
+        $.getJSON(prefixUri + '/refresh-expired-cache', function (data) {
+            if (data.refresh) {
+                $.gritter.add({
+                    title: "Mise à jour du cache",
+                    text: "Les données en cache ont été mises à jour.",
+                    class_name: "gritter-light"
+                });
+            }
+            setTimeout(refreshExpiredCache, 5000);
+        });
     };
 
     setTimeout(refreshExpiredCache, 5000);
+
+    $('#clear-cache').click(function () {
+        $.getJSON(prefixUri + '/clear-cache');
+    });
 });
