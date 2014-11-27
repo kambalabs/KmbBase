1a37db9a03f25f3ac6a371998f412ac1d2b2f59e
Emmanuel Bouton <goten4@gmail.com>
2014-11-27 12:39:52 +0100

    FIX datatables default settings + add nprogress

--- public/js/kmb.constants.js
+++ public/js/kmb.constants.js
@@ -1,10 +1,18 @@
-var DATATABLES_DEFAULT_SETTING = {
+var DATATABLES_DEFAULT_SETTINGS = {
     "pagingType": "bootstrap",
     "processing": true,
     "autoWidth": false,
     "order": [],
     "language": {
         "url": "/js/dataTables.json"
+    }
+};
+
+var DATATABLES_NPROGRESS_DEFAULT_SETTINGS = $.extend({}, DATATABLES_DEFAULT_SETTINGS, {
+    "fnPreDrawCallback": function() {
+        // gather info to compose a message
+        NProgress.start();
+        return true;
     },
     "drawCallback": function () {
         $('a', this.fnGetNodes()).each(function () {
@@ -15,6 +23,7 @@ var DATATABLES_DEFAULT_SETTING = {
                     hide: 200
                 }
             });
+            NProgress.done();
         });
     }
-};
+});
